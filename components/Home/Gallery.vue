5f3cb694a3ee9bab34ce6276dd143d6e4f5c820d
ichsanputr <ngekode24@gmail.com>
2024-08-05 23:02:48 +0700

    up

--- components/Home/Gallery.vue
+++ components/Home/Gallery.vue
@@ -20,50 +20,50 @@ onMounted(async () => {
         pswpModule: () => import('photoswipe'),
       });
       lightbox.value.on('uiRegister', function () {
-                      lightbox.value.pswp.ui.registerElement({
-                          name: 'download-button',
-                          order: 8,
-                          isButton: true,
-                          tagName: 'a',
-      
-                          // SVG with outline
-                          html: {
-                              isCustomSVG: true,
-                              inner: '<path d="M20.5 14.3 17.1 18V10h-2.2v7.9l-3.4-3.6L10 16l6 6.1 6-6.1ZM23 23H9v2h14Z" id="pswp__icn-download"/>',
-                              outlineID: 'pswp__icn-download'
-                          },
-      
-                          onInit: (el, pswp) => {
-                              el.setAttribute('download', '');
-                              el.setAttribute('target', '_blank');
-                              el.setAttribute('rel', 'noopener');
-      
-                              pswp.on('change', () => {
-                                  console.log('change');
-                                  el.href = pswp.currSlide.data.src;
-                              });
-                          }
-                      });
-      
-                      lightbox.value.pswp.ui.registerElement({
-                          name: 'custom-caption',
-                          order: 9,
-                          isButton: false,
-                          appendTo: 'root',
-                          html: 'Caption text',
-                          onInit: (el, pswp) => {
-                              lightbox.value.pswp.on('change', () => {
-                                  const currSlideElement = lightbox.value.pswp.currSlide.data.element;
-                                  let captionHTML = '';
-                                  if (currSlideElement) {
-                                      const hiddenCaption = currSlideElement.querySelector('.hidden-caption-content');
-                                      captionHTML = hiddenCaption.innerHTML;
-                                  }
-                                  el.innerHTML = "asasas";
-                              });
-                          }
-                      });
-                  });
+        lightbox.value.pswp.ui.registerElement({
+          name: 'download-button',
+          order: 8,
+          isButton: true,
+          tagName: 'a',
+
+          // SVG with outline
+          html: {
+            isCustomSVG: true,
+            inner: '<path d="M20.5 14.3 17.1 18V10h-2.2v7.9l-3.4-3.6L10 16l6 6.1 6-6.1ZM23 23H9v2h14Z" id="pswp__icn-download"/>',
+            outlineID: 'pswp__icn-download'
+          },
+
+          onInit: (el, pswp) => {
+            el.setAttribute('download', '');
+            el.setAttribute('target', '_blank');
+            el.setAttribute('rel', 'noopener');
+
+            pswp.on('change', () => {
+              console.log('change');
+              el.href = pswp.currSlide.data.src;
+            });
+          }
+        });
+
+        lightbox.value.pswp.ui.registerElement({
+          name: 'custom-caption',
+          order: 9,
+          isButton: false,
+          appendTo: 'root',
+          html: 'Caption text',
+          onInit: (el, pswp) => {
+            lightbox.value.pswp.on('change', () => {
+              const currSlideElement = lightbox.value.pswp.currSlide.data.element;
+              let captionHTML = '';
+              if (currSlideElement) {
+                const hiddenCaption = currSlideElement.querySelector('.hidden-caption-content');
+                captionHTML = hiddenCaption.innerHTML;
+              }
+              el.innerHTML = captionHTML;
+            });
+          }
+        });
+      });
       lightbox.value.init();
     }
   });
@@ -79,9 +79,10 @@ onMounted(async () => {
       <div id="gallery" class="rounded-lg grid grid-cols-1 md:grid-cols-3 mb-2 gap-6">
         <div v-for="(image, key) in images" :key="key" class="rounded-lg h-full w-full relative">
           <a :href="image.url" data-pswp-width="600" data-pswp-height="400" target="_blank" rel="noreferrer">
-                    <div class="hidden-caption-content">{{image.description}}</div>
+            <div class="hidden-caption-content">{{image.description}}</div>
             <v-img :lazy-src="image.url" class="rounded-md" cover width="100%" aspect-ratio="1" :src="image.url" />
-            <div class="rounded-lg z-10 py-1 backdrop-blur-xl opacity-90 pl-2 bg-[#0088CC] bottom-0 absolute w-full text-white">
+            <div
+              class="rounded-lg z-10 py-1 backdrop-blur-xl opacity-90 pl-2 bg-[#0088CC] bottom-0 absolute w-full text-white">
               <p class="truncate text-sm md:text-base">{{ image.description }}</p>
             </div>
           </a>
@@ -91,7 +92,30 @@ onMounted(async () => {
   </div>
 </template>
 
-<style scoped>
+<style>
+.pswp__custom-caption {
+  font-size: 16px;
+  color: #fff;
+  width: calc(100% - 32px);
+  max-width: 400px;
+  padding: 2px 8px;
+  border-radius: 4px;
+  text-align: center;
+  position: absolute;
+  left: 50%;
+  bottom: 16px;
+  transform: translateX(-50%);
+}
+
+.pswp__custom-caption a {
+  color: #fff;
+  text-decoration: underline;
+}
+
+.hidden-caption-content {
+  display: none;
+}
+
 ::v-deep img {
   border-radius: 6px;
   width: 100%;
--- pages/Galeri.vue
+++ pages/Galeri.vue
@@ -19,50 +19,50 @@ onMounted(async () => {
                 pswpModule: () => import('photoswipe'),
             });
             lightbox.value.on('uiRegister', function () {
-                            lightbox.value.pswp.ui.registerElement({
-                                name: 'download-button',
-                                order: 8,
-                                isButton: true,
-                                tagName: 'a',
-            
-                                // SVG with outline
-                                html: {
-                                    isCustomSVG: true,
-                                    inner: '<path d="M20.5 14.3 17.1 18V10h-2.2v7.9l-3.4-3.6L10 16l6 6.1 6-6.1ZM23 23H9v2h14Z" id="pswp__icn-download"/>',
-                                    outlineID: 'pswp__icn-download'
-                                },
-            
-                                onInit: (el, pswp) => {
-                                    el.setAttribute('download', '');
-                                    el.setAttribute('target', '_blank');
-                                    el.setAttribute('rel', 'noopener');
-            
-                                    pswp.on('change', () => {
-                                        console.log('change');
-                                        el.href = pswp.currSlide.data.src;
-                                    });
-                                }
-                            });
-            
-                            lightbox.value.pswp.ui.registerElement({
-                                name: 'custom-caption',
-                                order: 9,
-                                isButton: false,
-                                appendTo: 'root',
-                                html: 'Caption text',
-                                onInit: (el, pswp) => {
-                                    lightbox.value.pswp.on('change', () => {
-                                        const currSlideElement = lightbox.value.pswp.currSlide.data.element;
-                                        let captionHTML = '';
-                                        if (currSlideElement) {
-                                            const hiddenCaption = currSlideElement.querySelector('.hidden-caption-content');
-                                            captionHTML = hiddenCaption.innerHTML;
-                                        }
-                                        el.innerHTML = "asasas";
-                                    });
-                                }
-                            });
+                lightbox.value.pswp.ui.registerElement({
+                    name: 'download-button',
+                    order: 8,
+                    isButton: true,
+                    tagName: 'a',
+
+                    // SVG with outline
+                    html: {
+                        isCustomSVG: true,
+                        inner: '<path d="M20.5 14.3 17.1 18V10h-2.2v7.9l-3.4-3.6L10 16l6 6.1 6-6.1ZM23 23H9v2h14Z" id="pswp__icn-download"/>',
+                        outlineID: 'pswp__icn-download'
+                    },
+
+                    onInit: (el, pswp) => {
+                        el.setAttribute('download', '');
+                        el.setAttribute('target', '_blank');
+                        el.setAttribute('rel', 'noopener');
+
+                        pswp.on('change', () => {
+                            console.log('change');
+                            el.href = pswp.currSlide.data.src;
                         });
+                    }
+                });
+
+                lightbox.value.pswp.ui.registerElement({
+                    name: 'custom-caption',
+                    order: 9,
+                    isButton: false,
+                    appendTo: 'root',
+                    html: 'Caption text',
+                    onInit: (el, pswp) => {
+                        lightbox.value.pswp.on('change', () => {
+                            const currSlideElement = lightbox.value.pswp.currSlide.data.element;
+                            let captionHTML = '';
+                            if (currSlideElement) {
+                                const hiddenCaption = currSlideElement.querySelector('.hidden-caption-content');
+                                captionHTML = hiddenCaption.innerHTML;
+                            }
+                            el.innerHTML = captionHTML;
+                        });
+                    }
+                });
+            });
             lightbox.value.init();
         }
     });
@@ -119,18 +119,22 @@ useHead({
             <div class="grid grid-cols-1 md:grid-cols-3 gap-[2rem]">
                 <div v-for="(video, key) in videos" :key="key" class="w-full rounded-lg">
                     <a :href="video.url" target="_blank" rel="noreferrer">
-                        <iframe class="w-full rounded-t-lg shadow-sm" width="100%" height="245" loading="lazy" :src="video.url" title="YouTube video player"
-                            frameborder="0" allow="accelerometer; autoplay; web-share"
-                            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                        <iframe class="w-full rounded-t-lg shadow-sm" width="100%" height="245" loading="lazy"
+                            :src="video.url" title="YouTube video player" frameborder="0"
+                            allow="accelerometer; autoplay; web-share" referrerpolicy="strict-origin-when-cross-origin"
+                            allowfullscreen></iframe>
                     </a>
-                    <div class="rounded-b-lg py-3 px-2 font-medium text-base md:text-lg backdrop-blur-sm bg-white/30 shadow-sm border border-slate-100">
-                        <span v-if="video.description.length > 40 && $vuetify.display.mobile">{{ video.description.slice(0, 40) }}...</span>
+                    <div
+                        class="rounded-b-lg py-3 px-2 font-medium text-base md:text-lg backdrop-blur-sm bg-white/30 shadow-sm border border-slate-100">
+                        <span v-if="video.description.length > 40 && $vuetify.display.mobile">{{
+                            video.description.slice(0, 40)
+                            }}...</span>
                         <span v-else>{{ video.description }}</span>
                     </div>
                 </div>
             </div>
-            <v-pagination :size="$vuetify.display.mobile ? 'small' : 'default'" class="mt-4 mb-6 md:mb-10" v-model="page"
-                @update:modelValue="changePage" :total-visible="5" :length="pageLength"></v-pagination>
+            <v-pagination :size="$vuetify.display.mobile ? 'small' : 'default'" class="mt-4 mb-6 md:mb-10"
+                v-model="page" @update:modelValue="changePage" :total-visible="5" :length="pageLength"></v-pagination>
         </div>
         <div class="pb-[6rem]">
             <h1 class="mb-8 font-semibold text-[#0088CC] text-2xl">Galeri Foto</h1>
@@ -138,7 +142,7 @@ useHead({
                 <a class="w-full cursor-pointer rounded-lg" v-for="(image, key) in images" :key="key" :href="image.url"
                     data-pswp-width="600" data-pswp-height="400" target="_blank" rel="noreferrer">
                     <v-img :lazy-src="image.url" class="w-full rounded-t-lg" height="300" :src="image.url" alt="" />
-					                    <div class="hidden-caption-content">{{ image.description}}</div>
+                    <div class="hidden-caption-content">{{ image.description }}</div>
                     <div
                         class="rounded-b-lg py-3 px-2 font-medium text-base md:text-lg backdrop-blur-sm bg-white/30 shadow-sm border border-slate-100">
                         <span class="line-clamp-1">{{ image.description }}</span>
